da7cb101fe2fa7bbfff4c249cd7573a816eceb37
THEDEATHBOT <jafroth@gmail.com>
2017-01-25 16:29:26 -0800

    Hopefully all PID code integrated

--- src/org/usfirst/frc/team997/robot/RobotMap.java
+++ src/org/usfirst/frc/team997/robot/RobotMap.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team997.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -7,8 +14,30 @@ package org.usfirst.frc.team997.robot;
  * floating around.
  */
 public class RobotMap {
-	
-public static final int driveTrainLeftMotor, driveTrainRightMotor;
 
+public static Encoder leftEncoder;
+public static Encoder rightEncoder;
+public static Gyro gyro;
+
+public static void init() {
+	final double gearRatio = 0.225;
+	final double ticksPerRev = 2048;
+	final double pi = 3.14;
+	final double radius = 2;
+	final double calculated = ((gearRatio*2*pi)/ticksPerRev)*radius;
 	
+	gyro = new ADXRS450_Gyro(SPI.Port.valueOf("kOnboardCS0"));
+	
+	leftEncoder = new Encoder(0, 1, false, EncodingType.k4X);
+	leftEncoder.setDistancePerPulse(calculated);
+	leftEncoder.setPIDSourceType(PIDSourceType.kRate);
+	rightEncoder = new Encoder(2, 3, false, EncodingType.k4X);
+	rightEncoder.setDistancePerPulse(calculated);
+	rightEncoder.setPIDSourceType(PIDSourceType.kRate);
+}
+	
+public static final int 
+driveTrainLeftMotor = 0, 
+driveTrainRightMotor = 1;
+
 }
--- src/org/usfirst/frc/team997/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team997/robot/subsystems/DriveTrain.java
@@ -1,7 +1,12 @@
 package org.usfirst.frc.team997.robot.subsystems;
 
 import org.usfirst.frc.team997.robot.RobotMap;
+import org.usfirst.frc.team997.robot.commands.Drive;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -12,6 +17,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class DriveTrain extends Subsystem {
 	private Victor left;
 	private Victor right;
+	private Encoder leftEncoder = RobotMap.leftEncoder;
+	private Encoder rightEncoder = RobotMap.rightEncoder;
+	private final ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.valueOf("kOnboardCS0"));
+	
+	double pasterr = 0.01D;
+	double integral = 0.0D;
+	double speed = 0.0D;
+	
+	double rotpasterr = 0;
+	double rotintegral = 0;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 public void drive(double leftValue, double rightValue) {
@@ -20,14 +35,70 @@ public void drive(double leftValue, double rightValue) {
 	SmartDashboard.putNumber("DriveTrain left", -leftValue);
 	SmartDashboard.putNumber("DriveTrain right", rightValue);
 }
+
+public void driveToDistance(double setPoint) {
+	while(leftEncoder.getDistance() <= setPoint) {
+		double kpe = 0.5/setPoint;
+		double error = setPoint-leftEncoder.getDistance();
+		double derivative = error - pasterr;
+		pasterr = error;
+		speed = (kpe*error) + (0*integral) + (0*derivative);
+		
+		if(error > 0.01) {
+			integral = 0.0D;
+		} else {integral += error;}
+		
+		double kpg = 0.06;
+		double angle = RobotMap.gyro.getAngle();
+		
+		speed = Math.min(Math.max(speed, 0.5D), -0.5D);
+		drive(speed, -kpg*angle);
+		
+		Timer.delay(0.004);
+	}
+	
+}
+public void rotateToAngle(double angle) {
+	while(Math.abs(RobotMap.gyro.getAngle()) <= Math.abs(angle)) {
+		double maxSpeed = 0.25;
+		double currentAngle = RobotMap.gyro.getAngle();
+		double kpr = maxSpeed/angle;
+		double roterr = angle-currentAngle;
+		double kdr = kpr-rotpasterr;
+		rotpasterr = roterr;
+		
+		double curve = (kpr*roterr) + (0*rotintegral) + (5*kdr);
+		
+		if(roterr > 0.01) {
+			rotintegral = 0.0D;
+		} else {rotintegral += roterr;}
+		
+		curve = Math.min(Math.max(curve,  maxSpeed), -maxSpeed);
+		
+		if(angle > 0) {
+			drive(-curve, -curve);
+		} else {
+			drive(curve, curve);
+		}
+	}
+}
+
+
 public DriveTrain(){
 	left = new Victor(RobotMap.driveTrainLeftMotor);
 	right = new Victor(RobotMap.driveTrainRightMotor);
+	//leftEncoder = new Encoder(RobotMap.leftEncoder);
+	//rightEncoder = new Encoder(RobotMap.rightEncoder);
+	
 }
-    public void initDefaultCommand() {
     
+public void initDefaultCommand() {
+    
+    	setDefaultCommand(new Drive());
     	// Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    
 }
 
